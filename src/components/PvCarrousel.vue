f18825aa26e74dd073902068921ee8bc98b624e2
pouriversal <pooriversal@gmail.com>
2021-09-16 16:30:27 +0430

    fix last movement bug

--- src/components/PvCarrousel.vue
+++ src/components/PvCarrousel.vue
@@ -81,9 +81,9 @@ export default {
         return;
       }
       if (!this.loop && this.initIndex == this.lastIndex) {
+        // when there is a little of last card left (adding an extra push)!
         ++this.initIndex;
-        this.transformData +=
-          (1 - (this.slidesToShow % 1)) * (this.card_width - this.gap);
+        this.transformData += this.extra;
         this.transformVal = `translateX(${this.direction}px)`;
         return;
       }
@@ -102,16 +102,14 @@ export default {
       if ((this.rewind && this.transformData <= 0) || this.initIndex == 1) {
         this.initIndex = this.lastIndex + 1;
         this.transformData =
-          (this.lastIndex - 1) * (this.card_width + this.gap) +
-          (1 - (this.slidesToShow % 1)) * (this.card_width - this.gap);
+          (this.lastIndex - 1) * (this.card_width + this.gap) + this.extra;
         this.transformVal = `translateX(${this.direction}px)`;
         return;
       }
 
       if (this.initIndex > this.lastIndex) {
         --this.initIndex;
-        this.transformData -=
-          (1 - (this.slidesToShow % 1)) * this.card_width - this.gap;
+        this.transformData -= this.extra;
         this.transformVal = `translateX(${this.direction}px)`;
         return;
       }
@@ -124,12 +122,11 @@ export default {
       console.log(this.arr[this.arr.length - 1] - this.arr[0]);
       // console.log(this.arr)
       // ------------------------------------------------------------------------------------
-      this.mouseMove = this.arr[this.arr.length - 1] - this.arr[0];
+      this.mouseMove = this.arr[this.arr.length - 1] - this.arr[0]; // to check the direction of grabbing
       if (this.mouseMove > 0 && this.transformData !== 0) {
         if (this.initIndex > this.lastIndex) {
           --this.initIndex;
-          this.transformData -=
-            (1 - (this.slidesToShow % 1)) * this.card_width - this.gap;
+          this.transformData -= this.extra;
           this.transformVal = `translateX(${this.direction}px)`;
           return;
         }
@@ -187,15 +184,17 @@ export default {
   },
   computed: {
     slidePage() {
+      // think of it as dots in carousel
       return (
         this.overalLenght / (this.slidesToShow * (this.card_width - this.gap))
       );
     },
     slidesToShow() {
+      //how many slides can be seen in a viewport
       return this.container_width / (this.card_width + this.gap);
     },
     extra() {
-      return (1 - (this.slidesToShow % 1)) * this.card_width - this.gap;
+      return (1 - (this.slidesToShow % 1)) * (this.card_width - this.gap);
     },
     lastIndex() {
       return this.cardCount - Math.floor(this.slidesToShow);
